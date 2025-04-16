c6ebe727e85102332dfd312be8fcadd4b7ae6bcb
Ilya Grishanov <grishanov@me.com>
2025-04-16 14:11:50 +0300

    Fix: CoreDataStore final changes

--- Tracker/CoreData/CoreDataSource.swift
+++ Tracker/CoreData/CoreDataSource.swift
@@ -9,14 +9,35 @@ import CoreData
 import Foundation
 
 final class CoreDataSource {
+    
+    static let shared = CoreDataSource()
+    
     lazy var persistentContainer: NSPersistentContainer = {
-            let container = NSPersistentContainer(name: "Tracker")
+            let container = NSPersistentContainer(name: "TrackerModel")
             container.loadPersistentStores(completionHandler: { (storeDescription, error) in
                 if let error = error as NSError? {
-                    
+                    fatalError("Не удалось загрузить хранилище: \(error), \(error.userInfo)")
                 }
             })
             return container
         }()
     
+    var context: NSManagedObjectContext {
+            persistentContainer.viewContext
+        }
+
+    func saveContext() {
+            if context.hasChanges {
+                do {
+                    try context.save()
+                } catch {
+                    let nserror = error as NSError
+                    assertionFailure("Ошибка сохранения: \(nserror), \(nserror.userInfo)")
+                }
+            }
+        }
+        
+        private init() {}
+    
 }
+
