1ad4cfc2143fc853ecc94e2580ce2134231113d6
Ilya Grishanov <grishanov@me.com>
2025-04-27 23:34:08 +0300

    Add: Onboarding view controllers

---
+++ Tracker/OnboardingView/OnboardingViewController.swift
@@ -0,0 +1,97 @@
+//
+//  OnboardingViewController.swift
+//  Tracker
+//
+//  Created by Ilya Grishanov on 27.04.2025.
+//
+import UIKit
+
+class OnboardingViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    private lazy var goFurtherButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("Вот это технологии!", for: .normal)
+        button.setTitleColor(UIColor(named: "CustomWhite"), for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        button.layer.cornerRadius = 16
+        button.layer.borderWidth = 1
+        button.backgroundColor = UIColor(named: "CustomBlack")
+        button.addTarget(self, action: #selector(goFurtherButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var pages: [UIViewController] = {
+        let blueVC = BlueOnboardingVC()
+        let redVC = RedOnboardingVC()
+        return [blueVC, redVC]
+    }()
+
+    lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = pages.count
+        pageControl.currentPage = 0
+        pageControl.currentPageIndicatorTintColor = .black
+        pageControl.pageIndicatorTintColor = .gray
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        return pageControl
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+        delegate = self
+
+        if let first = pages.first {
+            setViewControllers([first], direction: .forward, animated: true, completion: nil)
+        }
+
+        view.addSubview(pageControl)
+        view.addSubview(goFurtherButton)
+
+        NSLayoutConstraint.activate([
+            pageControl.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            pageControl.bottomAnchor.constraint(equalTo: goFurtherButton.topAnchor, constant: -24),
+            
+            goFurtherButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            goFurtherButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            goFurtherButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            goFurtherButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+
+    @objc private func goFurtherButtonTapped() {
+        let mainTabBarController = MainTabBarController()
+        if let window = UIApplication.shared.windows.first {
+            window.rootViewController = mainTabBarController
+            UIView.transition(with: window, duration: 0.3, options: .transitionCrossDissolve, animations: nil)
+        }
+    }
+
+    // MARK: - UIPageViewControllerDataSource
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+        let previousIndex = viewControllerIndex - 1
+        guard previousIndex >= 0 else { return pages.last }
+        return pages[previousIndex]
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+        let nextIndex = viewControllerIndex + 1
+        guard nextIndex < pages.count else { return pages.first }
+        return pages[nextIndex]
+    }
+
+    // MARK: - UIPageViewControllerDelegate
+
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+
+        if let currentViewController = pageViewController.viewControllers?.first,
+           let currentIndex = pages.firstIndex(of: currentViewController) {
+            pageControl.currentPage = currentIndex
+        }
+    }
+}
--- Tracker/Resources/AppDelegate.swift
+++ Tracker/Resources/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
-        window?.rootViewController = MainTabBarController()
+        window?.rootViewController = OnboardingViewController()
         window?.makeKeyAndVisible()
         return true
     }
